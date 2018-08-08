347dce3099aa0a33609af5293b225f645f462a45
liutianyu@cmcm.com <liutianyu@cmcm.com>
2018-08-08 17:34:00 +0800

    add tabBar

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -15,23 +15,32 @@ Page({
                 content: "http://toupai.liebao.cn/external/5z2xPfIcWXy4MwhbZDFf6A_part_0.mp4",
                 title:"几十年前，刘德华与苏永康唱黄梅戏的画面，太经典了",
                 name: "挑战好奇心",
+            },
+            {
+                id: "03",
+                content: "http://toupai.liebao.cn/compact/e6M2bgklXI2bNyBEBL_R1A_part_0.mp4",
+                title: "激烈的赛车赛道 速度很性能都很强悍",
+                name: "汽车一手资讯"
             }
         ],
         relateContent:[
             {
-                videoImg:"http://img.v.cmcm.com/af60cb07-f560-533e-be9f-79775a0a6862_crop_200x140.jpg",
-                videoTitle:"贝爷在格鲁吉亚大高加索山二，绝对不要直接吃雪",
-                eye_num:"21344"
+                videoImg:"http://img.v.cmcm.com/toupaicms/04a99131-1265-cafc-6c13-aab8ab47569c_crop_750x424.png",
+                videoTitle:"IU - I Believe",
+                eye_num: "21344",
+                videoId: "01"
             },
             {
-                videoImg: "http://img.v.cmcm.com/c14b6d01-e767-5374-9f30-f2a44ff44b58_crop_200x140.jpg",
-                videoTitle: "贝爷在格鲁吉亚大高加索山三，不敢吃秃鹫蛋",
-                eye_num: "15688"
+                videoImg: "http://img.v.cmcm.com/0e0b0ca3-3cae-578b-b08f-b31cefa39fda_crop_750x424.jpg",
+                videoTitle: "几十年前，刘德华与苏永康唱黄梅戏的画面，太经典了",
+                eye_num: "15688",
+                videoId: "02"
             },
             {
-                videoImg: "http://img.v.cmcm.com/034fa0fc-bdd8-5cef-8692-4af5dcb5b0ff_crop_200x140.jpg",
-                videoTitle: "贝爷在格鲁吉亚大高加索山四，差点摔死",
-                eye_num: "42390"
+                videoImg: "http://img.v.cmcm.com/cadbf4f3-6f32-5254-9cad-1de3614818cd_crop_750x424.jpg",
+                videoTitle: "激烈的赛车赛道 速度很性能都很强悍",
+                eye_num: "42390",
+                videoId:"03"
             },
         ],
         
@@ -50,5 +59,11 @@ Page({
                 });
             }
         }
+    },
+    gotoDetail:function(event){
+        var videoid = event.currentTarget.dataset.videoid
+        wx.navigateTo({
+            url: '../detail/detail?videoid=' + videoid
+        })
     }
 })
